06019c540159343d01d799f5924e8f81f8e2d37e
Kyle Bradbury <kjb17@duke.edu>
2018-05-30 14:56:52 -0400

    updates

--- fibo.py
+++ fibo.py
@@ -16,3 +16,28 @@ def fib2(n):   # return Fibonacci series up to n
         result.append(b)
         a, b = b, a+b
     return result
+
+class BankCustomer:
+    '''A customer of a bank with a checking account.
+    
+    Attributes:
+        name: name of the customer (string)
+        balance: amount of money in the account (float)
+    
+    Credit: this class is adapted from an excellent example from Jeff Knupp: 
+        https://jeffknupp.com/
+    '''
+    
+    def __init__(self,name,balance=0.0):
+        self.name    = name
+        self.balance = balance
+
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print('Overdraft alert! Amount requested greater than available balance.')
+        self.balance -= amount
+        return self.balance
+        
+    def deposit(self,amount):
+        self.balance += amount
+        return self.balance
